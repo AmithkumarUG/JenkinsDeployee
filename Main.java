e72f3ecb0c4a72b6688fb5d65733a0e6fb8aaf01
AmithkumarUG <65775690+AmithkumarUG@users.noreply.github.com>
2024-11-09 20:38:59 +0530

    Added frequency
    Frequency of the chars found

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -33,5 +34,14 @@ public class Main {
         }
 
         Arrays.stream(str1).filter(e -> freqMap.get(e)==1).forEach(System.out::println);
+
+        //To Convert String to A Map and Printing it
+
+        System.out.println("------------------------------------");
+
+        Map<String, Long> strMap = Arrays.stream(str1).
+                collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        strMap.entrySet().stream().forEach(System.out::println);
     }
 }
